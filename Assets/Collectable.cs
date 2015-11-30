230840c3328ccbc191e80a16c04b23d532a4e68d
InzyWong <vwong@kth.se>
2015-11-30 17:03:13 +0100

    background sound and collectable coin with counter

---
+++ Assets/Collectable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+	
+	public Text counterText;
+	public int countSum;
+	public int coinValue=1;
+	void Update(){
+		//transform.Rotate (new Vector3 (0, 40, 0) * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.CompareTag ("Coin")) {;
+			//col.gameObject.SetActive(false);
+			Destroy (col.gameObject);
+			countSum +=coinValue;
+			counterText.text=countSum.ToString();
+			//Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Script/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+	
+	}
+}
